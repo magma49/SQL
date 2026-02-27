53f12770d4c33eef1f66e8d94d95af529483bb1f
ndwPC <magma0409@gmail.com>
2026-02-27 14:53:06 +0900

    feat: submit

--- Programmers/GROUP BY/성분으로 구분한 아이스크림 총 주문량/makeDB.sql
+++ Programmers/GROUP BY/성분으로 구분한 아이스크림 총 주문량/makeDB.sql
@@ -1,10 +1,49 @@
-CREATE TABLE
-    IF NOT EXISTS (INTEGER NOT NULL, VARCHAR(255) NOT NULL);
+DROP TABLE IF EXISTS ICECREAM_INFO;
+DROP TABLE IF EXISTS FIRST_HALF;
 
-INSERT INTO
-VALUES
-    ();
+CREATE TABLE FIRST_HALF (
+    SHIPMENT_ID INT NOT NULL,
+    FLAVOR VARCHAR(50) NOT NULL PRIMARY KEY,
+    TOTAL_ORDER INT NOT NULL
+);
+
+CREATE TABLE ICECREAM_INFO (
+    FLAVOR VARCHAR(50) NOT NULL PRIMARY KEY,
+    INGREDIENT_TYPE VARCHAR(50) NOT NULL,
+    FOREIGN KEY (FLAVOR) REFERENCES FIRST_HALF(FLAVOR)
+);
+
+INSERT INTO FIRST_HALF (SHIPMENT_ID, FLAVOR, TOTAL_ORDER) VALUES
+(101, 'chocolate', 3200),
+(102, 'vanilla', 2800),
+(103, 'mint_chocolate', 1700),
+(104, 'caramel', 2600),
+(105, 'white_chocolate', 3100),
+(106, 'peach', 2450),
+(107, 'watermelon', 2150),
+(108, 'mango', 2900),
+(109, 'strawberry', 3100),
+(110, 'melon', 3150),
+(111, 'orange', 2900),
+(112, 'pineapple', 2900);
+
+INSERT INTO ICECREAM_INFO (FLAVOR, INGREDIENT_TYPE) VALUES
+('chocolate', 'sugar_based'),
+('vanilla', 'sugar_based'),
+('mint_chocolate', 'sugar_based'),
+('caramel', 'sugar_based'),
+('white_chocolate', 'sugar_based'),
+('peach', 'fruit_based'),
+('watermelon', 'fruit_based'),
+('mango', 'fruit_based'),
+('strawberry', 'fruit_based'),
+('melon', 'fruit_based'),
+('orange', 'fruit_based'),
+('pineapple', 'fruit_based');
 
 SELECT
     *
-FROM;
+FROM FIRST_HALF;
+SELECT
+    *
+FROM ICECREAM_INFO;
