4b30b5056b167cb0f04b6a176a60bf7c8a61dee8
ndwPC <magma0409@gmail.com>
2026-02-27 15:09:26 +0900

    feat: submit 조건 자세히

--- Programmers/GROUP BY/진료과별 총 예약 횟수 출력하기/solution.sql
+++ Programmers/GROUP BY/진료과별 총 예약 횟수 출력하기/solution.sql
@@ -1,1 +1,1 @@
-SELECT `MCDP_CD` '진료과코드',COUNT(*) '5월예약건수' FROM appointment GROUP BY MCDP_CD ORDER BY COUNT(*);
+SELECT `MCDP_CD` '진료과코드',COUNT(*) '5월예약건수' FROM appointment WHERE DATE_FORMAT(`APNT_YMD`,'%Y-%m')= '2022-05' GROUP BY MCDP_CD ORDER BY COUNT(*),MCDP_CD;
