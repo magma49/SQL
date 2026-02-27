b8c97bc296ee4a6c7f30a8fd545313309747c353
ndwPC <magma0409@gmail.com>
2026-02-27 15:18:20 +0900

    feat: submit

--- Programmers/GROUP BY/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/makeDB.sql
+++ Programmers/GROUP BY/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/makeDB.sql
@@ -1,10 +1,21 @@
-CREATE TABLE
-    IF NOT EXISTS (INTEGER NOT NULL, VARCHAR(255) NOT NULL);
+-- 1. 테이블 초기화 (항상 맨 앞에 추가)
+DROP TABLE IF EXISTS CAR_RENTAL_COMPANY_CAR;
 
-INSERT INTO
-VALUES
-    ();
+-- 2. 테이블 생성 (명세서에 PK 언급이 없으므로 생략)
+CREATE TABLE CAR_RENTAL_COMPANY_CAR (
+    CAR_ID INTEGER NOT NULL,
+    CAR_TYPE VARCHAR(255) NOT NULL,
+    DAILY_FEE INTEGER NOT NULL,
+    OPTIONS VARCHAR(255) NOT NULL
+);
 
-SELECT
-    *
-FROM;
+-- 3. 데이터 삽입
+INSERT INTO CAR_RENTAL_COMPANY_CAR (CAR_ID, CAR_TYPE, DAILY_FEE, OPTIONS) VALUES
+(1, '세단', 16000, '가죽시트,열선시트,후방카메라'),
+(2, 'SUV', 14000, '스마트키,네비게이션,열선시트'),
+(3, 'SUV', 22000, '주차감지센서,후방카메라'),
+(4, '트럭', 35000, '주차감지센서,네비게이션,열선시트'),
+(5, 'SUV', 16000, '가죽시트,네비게이션,열선시트,후방카메라,주차감지센서');
+
+-- 4. 삽입된 데이터 최종 확인
+SELECT * FROM CAR_RENTAL_COMPANY_CAR;
---
+++ Programmers/GROUP BY/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/solution.sql
@@ -0,0 +1,1 @@
+SELECT `CAR_TYPE`, COUNT(*) CARS FROM car_rental_company_car WHERE `OPTIONS` LIKE '%가죽시트%' OR`OPTIONS` LIKE '%열선시트%'OR`OPTIONS` LIKE '%통풍시트%' GROUP BY `CAR_TYPE` order by CAR_TYPE; 
