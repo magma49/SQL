806d0092bd44c571c38b3a296026848e449cf751
ndwPC <magma0409@gmail.com>
2026-02-28 01:14:30 +0900

    feat: submit 조건 자세히

--- Programmers/String, Date/중성화 여부 파악하기/solution.sql
+++ Programmers/String, Date/중성화 여부 파악하기/solution.sql
@@ -1,7 +1,12 @@
 SELECT
     ANIMAL_ID,
     NAME,
-    IF(SEX_UPON_INTAKE LIKE 'Neutered%', 'O', 'X') '중성화'
+    IF(
+        SEX_UPON_INTAKE LIKE 'Neutered%'
+        OR SEX_UPON_INTAKE LIKE 'Spayed%',
+        'O',
+        'X'
+    ) '중성화'
 FROM
     ANIMAL_INS
 ORDER BY
